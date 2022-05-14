aae27ca08c8d05e1ae2bf8fe9fe5061b6688719e
Ondrej <ocky852@gmail.com>
2022-05-14 18:29:20 +0200

    Improve building script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+CHIPSET_NAME=lahaina
 export ARCH=arm64
-export PROJECT_NAME=q2q
+export PLATFORM_VERSION=12
+export ANDROID_MAJOR_VERSION=s
 mkdir out
 
-BUILD_CROSS_COMPILE=~/kernel/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+BUILD_CROSS_COMPILE=~/kernel/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 KERNEL_LLVM_BIN=~/kernel/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
@@ -12,4 +14,10 @@ KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y vendor/r9q_eur_openx_papaya_defconfig
 make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y
  
-#cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+mkdir out/G990B
+
+cp out/arch/arm64/boot/Image $(pwd)/out/G990B/Image
+
+DTBO_FILES=$(find $(pwd)/out/arch/$ARCH/boot/dts/samsung/ -name r9q_*_w00_r*.dtbo)
+    cat $(pwd)/out/arch/$ARCH/boot/dts/vendor/qcom/*.dtb > $(pwd)/out/G990B/dtb.img
+    $(pwd)/tools/mkdtimg create $(pwd)/out/G990B/dtbo.img --page_size=4096 ${DTBO_FILES}
